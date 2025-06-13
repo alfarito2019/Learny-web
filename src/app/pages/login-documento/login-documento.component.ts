3d335a4c1443afbaa91e7d0f5757aafb4395f510
alfarito2019 <joalfaroc@unal.edu.co>
2025-06-13 10:13:55 -0500

    ya está el front del welcome y del login

--- src/app/pages/login-documento/login-documento.component.ts
+++ src/app/pages/login-documento/login-documento.component.ts
@@ -1,12 +1,46 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login-documento',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './login-documento.component.html',
-  styleUrl: './login-documento.component.scss'
+  styleUrls: ['./login-documento.component.scss']
 })
 export class LoginDocumentoComponent {
+  tipoDocumentoOptions = [
+    'Cédula de ciudadanía',
+    'Tarjeta de identidad',
+    'Cédula de extranjería'
+  ];
+  selectedTipo: string = this.tipoDocumentoOptions[0];
+  numeroDocumento: string = '';
+  validDocumento: boolean = false;
 
+  constructor(private router: Router) {}
+
+  onTipoChange(value: string) {
+    this.selectedTipo = value;
+  }
+
+  onNumeroChange(value: string) {
+    this.numeroDocumento = value;
+    // Simulación de verificación: solo números con al menos 6 dígitos
+    this.validDocumento = /^[0-9]{6,}$/.test(value);
+  }
+
+  goBack() {
+    this.router.navigate(['/welcome']);
+  }
+
+  goNext() {
+    if (this.validDocumento) {
+      this.router.navigate(['/login/clave'], {
+        queryParams: { cedula: this.numeroDocumento }
+      });
+    }
+  }
 }
